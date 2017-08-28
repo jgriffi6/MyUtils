12f0da64f825d1863b7192d8796b2722a2ae4947
Justin Griffiths <jgriffi6@gmail.com>
2017-08-28 13:48:20 -0500

    commit

--- PlottingUtils.py
+++ PlottingUtils.py
@@ -1,38 +1,145 @@
 global plt
+plt=None
 
 def Plt(batch=False):
     import matplotlib
-    if batch:
-        matplotlib.use('Agg')
-        pass
     global plt
-    import matplotlib.pyplot as plt
+    if plt is not None: return plt
+    if batch:
+        print('running in batch mode')
+        matplotlib.use('Agg')
+        #import matplotlib.pyplot as plt
+        import pylab as plt
+        pass
+    else :
+        import pylab as plt
     return plt
 
 
 class Plotter():
 
-    def __init__(self, name, batch=True):
-        self.plt=Plt(batch)
-        self.name=name
-        fig,ax=plt.subplots()
-        self.fig=fig
-        self.ax=ax
-        self.d={}
+    def __init__(self, name=None, batch=True, from_pickle=None,labels_renew=None,labels_cancel=None):
+        if from_pickle is not None:
+            f=open(from_pickle, 'rb')
+            import pickle
+            d=pickle.load(f)
+            self.__dict__=d
+            f.close()
+            self.plt=Plt(batch)
+            pass
+        else :
+            self.plt=Plt(batch)
+            self.name=name
+            fig,ax=self.plt.subplots()
+            self.fig=fig
+            self.ax=ax
+            self.d={}
+            self.ax_right=None
+            self.labels_renew=labels_renew  ###Todo move
+            self.labels_cancel=labels_cancel  ###Todo move
+            pass
         pass
 
+    # def __html__(self):
+    #     return 
+
+    def pickle(self, name=None):
+        d={}
+        d['name']=self.name
+        d['fig']=self.fig
+        d['ax']=self.ax
+        d['d']=self.d
+        d['ax_right']=self.ax_right
+        d['labels_cancel']=self.labels_cancel
+        d['labels_renew']=self.labels_renew
+        import pickle
+        if name is None: name=self.name+'.pickle'
+        f=open(name,'wb')
+        pickle.dump(d,f)
+        f.close()
+        return
+    
+    def text(self,x,y,text,color='black',verticalalignment='bottom',horizontalalignment='left',
+             transform=None,fontsize=14):
+        if transform is None: transform=self.ax.transAxes
+        self.ax.text(x,y,text,color=color,verticalalignment=verticalalignment,horizontalalignment=horizontalalignment,
+                     transform=transform,fontsize=fontsize)
+        pass
+    
     def close(self):
         self.plt.close(self.fig)
         pass
 
-    def plot(self, x, y, label="_", linestyle="-", color=None):
+    def set_axis_labels(self,x=None,y=None,y2=None):
+        if x is not None: self.ax.set_xlabel(x)
+        if y is not None: self.ax.set_ylabel(y)
+        if y2 is not None: self.ax_right.set_ylabel(y2)
+    
+    def plot(self, x, y, label="_", linestyle="-", color=None, yaxis_label=None, log=False, kwargs={}):
+            
         if color is not None and color in self.d:
             color=self.d[color].get_color()
             pass
-        self.ax.plot(x,y,label=label,linestyle=linestyle, color=color)
+        fnc=self.ax.plot
+        if log: fnc=self.ax.semilogy
+        fnc(x,y,label=label,linestyle=linestyle, color=color, **kwargs)
         self.d[label]=self.ax.get_lines()[-1]
+        if yaxis_label is not None:
+            self.ax.set_ylabel(yaxis_label, color=color,fontsize='large')
+            pass
         pass
 
+    def normed_hist(self, x, bins=10, label='_', linestyle='-', color='b', yaxis_label=None,log=False):
+        if color is not None and color in self.d:
+            color=self.d[color].get_color()
+            pass
+        data,bin_edges=self.plt.histogram(x,bins)
+
+        if log:
+            print('log mode is not correct')
+            data2=[]
+            bin_edges2=[]
+            last_edge=bin_edges[min(1,len(bin_edges)-1)]
+            for i in range(len(data)):
+                if data[i]>0:
+                    data2.append(data[i])
+                    bin_edges2.append(bin_edges[i])
+                    last_edge=bin_edges[min(i,len(bin_edges)-1)]
+                    pass
+                continue
+            import math
+            import numpy as np
+            data=np.log(data2)/math.log(10)
+            bin_edges=np.array(bin_edges2+[last_edge])
+            pass
+
+        import numpy as np
+        bin_centers=(bin_edges[:-1]+bin_edges[1:])/2
+        data=data/data.sum()
+        result=data.copy()
+        data=np.append(np.append(data[0],data),data[-1])
+
+        bin_centers=np.append(np.append(bin_edges[0],bin_centers),bin_edges[-1])
+        self.ax.step(bin_centers,data,label=label,linestyle=linestyle,color=color)
+        #self.ax.plot(bin_centers,data,label=label,linestyle=linestyle,color=color)
+        
+        if yaxis_label is not None: self.ax.set_ylabel(yaxis_label,color=color,fontsize='large')
+        return result
+    
+    def plot_right(self, x, y, label="_", linestyle='-', color=None,yaxis_label=None,log=False, kwargs={}):
+        if self.ax_right is None:
+            self.ax_right=self.ax.twinx()
+            pass
+        for i,j in ({'label':label,'linestyle':linestyle,'color':color}).items():
+            kwargs[i]=j
+            continue
+        if not log : self.ax_right.plot(x,y,**kwargs)
+        else : self.ax_right.semilogy(x,y,**kwargs)
+        if yaxis_label is not None:
+            self.ax_right.set_ylabel(yaxis_label, color=color,fontsize='x-large')
+            pass
+        pass
+    
     def xLabelDateFormat(self,DateFormatter=None):
         import datetime
         import numpy as np
@@ -64,23 +171,143 @@ class Plotter():
         legend2=self.ax.legend()
         self.plt.gca().add_artist(legend2)
         pass
+
+    def set_limits(self, axis='y', minx=None, maxx=None, minr=None, maxr=None):
+        """
+        axis: 'x' or 'y'
+        minx/maxx: min and max values
+        minr/maxr: multiply min/max by some ratio
+        """
+        setter=getattr(self.ax, 'set_'+axis+'lim')
+        getter=getattr(self.ax, 'get_'+axis+'lim')
+        cmin,cmax=getter()
+        if minx is not None: cmin=minx
+        if maxx is not None: cmax=maxx
+        if minr is not None: cmin*=minr
+        if maxr is not None: cmax*=maxr
+        setter(cmin,cmax)
+        if (minr is not None or maxr is not None) and self.ax_right is not None:
+            setter=getattr(self.ax_right, 'set_'+axis+'lim')
+            getter=getattr(self.ax_right, 'get_'+axis+'lim')
+            cmin,cmax=getter()
+            if minr is not None: cmin*=minr
+            if maxr is not None: cmax*=maxr
+            setter(cmin,cmax)
+            pass            
+        pass
+
+    def is_scatter(self):
+        return len(self.ax.collections)>0
+
+    def is_line(self):
+        return not self.is_scatter() and len(self.ax.get_lines())
+
+    def asbokeh_scatter(self,hover_names=None,tools="pan,box_zoom,wheel_zoom,reset,hover,save,zoom_in,zoom_out",colors=None):
+        from bokeh.models import (
+            ColumnDataSource,
+            HoverTool,
+            LogColorMapper
+        )           
+        from bokeh.io import show, save
+        from bokeh.plotting import figure, output_file
+        from bokeh.embed import components
+        from bokeh.models import LinearAxis, Range1d, Span
+
+        p=figure(title=self.name, tools=tools)
+        if colors==None: colors=['red','blue','green','gray','orange']
+        for i in range(len(self.ax.collections)):
+            col=self.ax.collections[i]
+            source=ColumnDataSource(data=dict(
+                x=[x[0] for x in col.get_offsets()],
+                y=[x[1] for x in col.get_offsets()],
+            ))
+            if len(hover_names)==len(self.ax.collections):
+                #print(type(source.data))
+                #source.data[hover_names[i][0]]=hover_names[i][1]
+                source.add(hover_names[i][1], name=hover_names[i][0])
+                pass
+            p.circle('x','y',source=source,color=colors[i],legend=col.get_label())
+            continue
+
+        p.xaxis.axis_label=self.ax.get_xlabel()
+        p.yaxis.axis_label=self.ax.get_ylabel()
+
+        hover = p.select_one(HoverTool)
+        hover.point_policy = "follow_mouse"
+        hover.tooltips = [
+            ('x','@x'),
+            ('y','@y'),            
+        ]
+        if len(hover_names)==len(self.ax.collections):
+            hover.tooltips.append( (hover_names[0][0],'@'+hover_names[0][0]) )
+            pass
+        
+        return p
+
+    def asbokeh_plot(self):
+        from bokeh.models import (
+            ColumnDataSource,
+            HoverTool,
+            LogColorMapper
+        )           
+        from bokeh.io import show, save
+        from bokeh.plotting import figure, output_file
+        from bokeh.embed import components
+        from bokeh.models import LinearAxis, Range1d, Span
+
+        pass
     
-    def savefig(self, name=None, suf=[],legend=True):
+    def asbokeh(self,**kwargs):
+        if self.is_scatter() : return self.asbokeh_scatter(**kwargs)
+        elif self.is_line() : return self.asbokeh_plot(**kwargs)
+        else : raise RuntimeError("Not sure how to convert this matplotlib object")
+        
+    
+    def savefig(self, name=None, suf=[],legend=True,bbox_inches='tight',close=True,ftype=None, **kwargs):
+
+        if ftype is not None and ftype=='bokeh':
+            from bokeh.io import show, save
+            from bokeh.plotting import figure, output_file
+            p=self.asbokeh(**kwargs)
+            output_file(self.name+'.html')
+            save(p)
+            return 
+        
         if name is None: name=self.name
 
-        if legend : self.ax.legend()
+        if legend :
+            if self.ax_right is not None:
+                lines=self.ax.get_lines()
+                lines+=self.ax_right.get_lines()
+                lines_w_labels=[]
+                for l in lines:
+                    #if len(l.get_label()) and l.get_label()[0]!='_': lines_w_labels.append(l)
+                    if len(l.get_label()) and l.get_label()[0]=='_': continue
+                    else : lines_w_labels.append(l)
+                    continue
+                labels=[l.get_label() for l in lines_w_labels]
+                self.ax.legend(lines_w_labels, labels, **kwargs)
+                pass
+            else : self.ax.legend()
+            pass
         
         if len(suf)==0 :
             if not name.count('.'):name+='.png'
-            self.fig.savefig(name)
+            import os
+            #os.system('rm -f %s' %name)
+            # print('saving ', name)
+            # for l in self.ax.lines:
+            #     print(l._x, l._y)
+            self.fig.savefig(name,bbox_inches=bbox_inches)
             pass
         else:
             for suffix in suf:
-                self.fig.savefig(name+suf)
+                if not suffix.count('.'): suffix='.'+suffix
+                self.fig.savefig(name+suffix,bbox_inches=bbox_inches)
                 continue
             pass
-        self.close()
-        return
+        if close : self.close()
+        return name
             
 
     pass
@@ -94,7 +321,7 @@ class StackPlotter(Plotter):
         self._x=None
         pass
 
-    def plot(self, x, y, label="_", color=None,fill=True):
+    def plot(self, x, y, label="_", color=None,fill=True,**kwargs):
         last=np.zeros(len(y))
         if self._x==None:
             self._x=x
@@ -102,6 +329,7 @@ class StackPlotter(Plotter):
         else:
             last=self.total.copy()
             if x!=self._x: raise ValueError( "inconsistent x-axes ", x, "was ", self._x)
+            #print(self.total, "::\n", y)
             self.total+=np.array(y)
             pass
 
@@ -110,8 +338,13 @@ class StackPlotter(Plotter):
         # else:
         #     self.ax.plot(self._x,self.total,label=label)
         #     pass
-        self.ax.plot(self._x,self.total,label=label,color=color)
-        if fill : self.ax.fill_between(self._x,last,self.total)
+        step=None
+        if 'drawstyle' in kwargs:
+            step=kwargs['drawstyle'].replace('steps-','')
+            pass
+        print(step,kwargs)
+        self.ax.plot(self._x,self.total,label=label,color=color,**kwargs)
+        if fill : self.ax.fill_between(self._x,last,self.total,step=step,color=color)
         pass
     pass
                 
--- df_utils.py
+++ df_utils.py
@@ -1,6 +1,16 @@
 global pd
 import pandas as pd
 
+def Pd():
+    import pandas as pd
+    import os
+    columns=100
+    try:
+        rows,columns=os.popen('stty size', 'r').read().split()
+    except: pass
+    pd.options.display.width=int(columns)
+    return pd
+
 def select(df, criteria, columns=None):
     df2=df[df.eval(criteria)]
     if columns is not None:
@@ -19,7 +29,9 @@ def robust_column_names(df):
     import re
     reg_str='[-+ *&/\\@]'
     r=re.compile(reg_str)
+    new_cols=[]
     for col in df.columns:
+        new_cols.append(col)
         if col=='Unnamed: 0' : continue
         original_name=col
         result=r.search(col)
@@ -28,11 +40,13 @@ def robust_column_names(df):
             result=r.search(col)
             continue
         if original_name != col : d[original_name]=col
+        new_cols[-1]=col
         continue
 
-
-    df.rename(index=str, columns=d, inplace=True)
-
+    #for some reason this first method takes a long time...
+    #df.rename(index=str, columns=d, inplace=True, copy=False)
+    df.columns=new_cols
+    
     inverted_d={}
     for a,b in d.items():
         inverted_d[b]=a
@@ -92,7 +106,29 @@ def drop_unnamed(df):
         continue
     df.drop(cols, axis=1, inplace=True)
 
-def read_csv(fname, parse_dates=True, ignore_date_cols=[], do_robust_column_names=True, encoding='utf-8', quiet=False, append_to=None):
+def read_csv(fname, parse_dates=True, ignore_date_cols=[], do_robust_column_names=True, encoding='utf-8', quiet=False, append_to=None, selection=None, sep=',',usecols=None,strip_blank_space=None):
+    """
+    return a dataframe
+
+    fname: file name, or list of file names
+    
+    parse_dates: attempt to parse dates, look for 'date' in column name (case-insensitive), or for data in first row of column
+    resembling: xx:yy:zz
+
+    ignore_date_cols: in parsing of dates, ignore specific columns.  e.g. dates is a fruit ...
+
+    do_robust_column_names: convert spaces, and unfriendly symbols to '_'
+    
+    encoding: default 'utf-8'
+
+    append_to: provide a df that should be appended to output
+
+    selection: function or tuple(function, arg1, arg2, etc.)  df is always passed as first argument to function.
+    called for each df
+
+    strip_blank_space: default None, if list, do df[col]=df[col].map(lambda x: x.strip()) for all col in list, else if not None, strip all leading,trailing
+    white space for all columns with 'dtype=='O''
+    """
     date_cols=[]
     import re
     reg=re.compile('[0-9]{2}:[0-9]{2}:[0-9]{2}')
@@ -102,7 +138,7 @@ def read_csv(fname, parse_dates=True, ignore_date_cols=[], do_robust_column_name
     else: files=fname
     for fname in files:
         if parse_dates:
-            df=pd.read_csv(fname,nrows=1,encoding=encoding)
+            df=pd.read_csv(fname,nrows=1,encoding=encoding,sep=sep,usecols=usecols)
             for col in df.columns:
                 if col.lower().count('date') and not col in ignore_date_cols:
                     date_cols.append(col)
@@ -112,12 +148,20 @@ def read_csv(fname, parse_dates=True, ignore_date_cols=[], do_robust_column_name
                     date_cols.append(col)
                     pass
                 continue
-            df=pd.read_csv(fname, parse_dates=date_cols, infer_datetime_format=True, encoding=encoding)
+            df=pd.read_csv(fname, parse_dates=date_cols, infer_datetime_format=True, encoding=encoding,sep=sep,usecols=usecols)
             pass
         else:
-            df=pd.read_csv(fname,encoding=encoding)
+            df=pd.read_csv(fname,encoding=encoding,sep=sep,usecols=usecols)
             pass
         import resource
+        if selection is not None:
+            if type(selection)==tuple:
+                df=selection[0](df, *(selection[1:]))
+            else :
+                df=selection(df)
+                pass
+            pass
+            
         if append_to is not None:
             append_to=append_to.append(df, ignore_index=True)
             df=append_to
@@ -125,11 +169,145 @@ def read_csv(fname, parse_dates=True, ignore_date_cols=[], do_robust_column_name
         else: append_to=df
         mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         if not quiet: print('read in:', fname, ' total used memory: ', mem/1000, 'MB; rows=', len(df))
-        if do_robust_column_names: robust_column_names(df)
         continue
-    drop_unnamed(df)        
+    if do_robust_column_names: robust_column_names(df)
+    drop_unnamed(df)
+    if strip_blank_space is not None:
+        cols=df.columns
+        if type(strip_blank_space)==list: cols=strip_blank_space
+        elif type(strip_blank_space)==bool and strip_blank_space==False: cols=[]
+        for c in cols:
+            if df[c].dtype != 'O': continue
+            df[c]=df[c].map(lambda x: x.strip())
+            continue
+        pass
     return df
         
 def to_csv(df, fname):
     df.to_csv(fname, index=False)
     pass
+
+def general_dict(df, key=None, cols=None):
+    """
+    Note, probably could do some sort of join operation...
+    return dict[key] = df[df.key==value][cols].values
+    so a dict[key] = np.array(cols)
+    
+    key: str
+    cols: [str,str,...]
+
+    """
+    import numpy as np
+    d={}
+    #i=0
+    the_cols=[key]
+    the_cols+=cols
+    df_view=df[the_cols]
+    
+    for t in df_view.itertuples():
+        k=t[1]
+        if k in d:
+            d[k]=np.vstack((d[k], np.array(t[2:])))
+        else:
+            d[k]=np.array(t[2:])
+            pass
+        continue
+    return d
+
+def pairwise_dict(df, selection=None, cols=None, key=None, value=None):
+    if selection is not None: df=select(df,criteria=selection,columns=cols)
+    elif cols is not None: df=df[cols]
+
+    if len(df.columns)!=2 and (key is None or value is None):
+        raise RuntimeError('pairwise dict expects two columns!, dataframe has', len(df.columns), 'columns')
+
+    if key is None: key=df.columns[0]
+    if value is None: value=df.columns[1]
+
+    keys=df[key].tolist()
+    values=df[value].tolist()
+    d={}
+    for k,v in zip(keys,values): d[k]=v
+    return d
+
+def append_var(df, selection=None, groupby=None, new_var='', var='', transform='sum', transform2='max', default_val=-1):
+    """
+df: dataframe
+selection: string, pd.core.series.Series().  If string, df[df.eval(selection)], else, df[selection]... default: None
+transform2: 'max', 'min', None. What to do for non-matching rows after initial transform.  i.e. df.loc[ all, 'new_var']=df.groupby(groupby)[new_var].transform('transform2')
+you are selecting the 'transform2'
+default_val: default value
+groupby: list. List of items to group by
+var: name of var we are transforming
+transform: how to transform var
+new_var: name of new var
+    """
+    dummy=[True for i in range(len(df))]
+    if selection is None: selection=dummy
+    elif type(selection)==str:
+        if len(selection):selection=df.eval(selection)
+        else:selection=dummy
+        pass
+
+    if groupby is None or len(groupby)==0: raise ValueError('groupby must be a non-empty list')
+    if var is None or len(var)==0: raise ValueError('var must be a non-empty string')
+    if new_var is None or len(new_var)==0: raise ValueError('new_var must be a non-empty string')
+
+    if default_val is not None:df[new_var]=default_val
+
+    df.loc[selection, new_var]=df[selection].groupby(groupby)[var].transform(transform)
+    if transform2 is not None and len(transform2):
+        df.loc[ dummy, new_var]=df.groupby(groupby)[new_var].transform(transform2)
+        pass
+    
+    pass    
+
+def change_col_name(df, old_names, new_names):
+    """
+    old_names: str or list
+    new_names: str or list--must match old_names
+    """
+    #print("changing col name", old_names, new_names)
+    if type(old_names) != type(new_names):
+        raise ValueError('type of old_names and new_names do not match')
+    if type(old_names)==list:
+        if len(old_names) != len(new_names):
+            raise ValueError('len of old_names and new_names do not match', len(old_names), len(new_names))
+        for o,n in zip(old_names,new_names):
+            change_col_name(df,o,n)
+            continue
+        pass
+    else :
+        df.columns=[ new_names if col==old_names else col for col in df.columns ]
+        pass
+    return
+
+
+import time
+
+def parse_date(df, date_col, form=None, new_name=None):
+    if form is not None:
+        import timeit
+        def _temp_func_(x,form=form):
+            #if x!=x: return pd.Timestamp('2000-01-01')
+            if type(x)==float : return pd.Timestamp('2000-01-01')
+            a=time.strptime(x,form)
+            result=pd.Timestamp( a.tm_year, a.tm_mon, a.tm_mday )
+            return result
+        if type(form)==type(_temp_func_): _temp_func_=form
+        df[date_col]=df[date_col].map(lambda x: _temp_func_(x))
+    else:
+        df[date_col]=df[date_col].map(lambda x: pd.Timestamp(x))        
+        pass
+
+    if new_name is not None:
+        change_col_name(df, date_col, new_name)
+        pass
+    pass
+
+
+def denan_cols(df, cols, value=0):
+    for col in cols:
+        df[col]=df[col].map(lambda x: x if x==x else value)
+        pass
+    return
